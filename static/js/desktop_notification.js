af7e7eebefd4434a94f05b046df5f5815c9c886d
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2022-11-11 00:33:02 -0500

    Added button on templates/exercise_details.html so that there can be a user gesture to spawn desktop notifications.

--- static/js/desktop_notification.js
+++ static/js/desktop_notification.js
@@ -1,11 +1,20 @@
-if ('Notification' in window) {
-    if (Notification.permission === 'granted') {
-        const notification = new Notification('Test!');
-    } else if (Notification.permission !== 'denied') {
-        Notification.requestPermission().then((permission) => {
-            if (permission === 'granted') {
-                const notification = new Notification('Test me');
-            }
-        });
+'use strict';
+
+const button = document.querySelector('#notify'); 
+
+button.addEventListener('click', (evt) => {
+    if ('Notification' in window) {
+        if (Notification.permission === 'granted') {
+            const notification = new Notification('Test!');
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission().then((permission) => {
+                if (permission === 'granted') {
+                    const notification = new Notification('Test me');
+                }
+            });
+        }
     }
-  }
+});
+
+// Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')
+// This file is loading in the <head> and the id #notify is not yet found
