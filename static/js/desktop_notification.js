10c872c468ad42b68536d6fd1550d9b6f0d2882e
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2022-11-13 23:52:37 -0500

    Tested that we can use setInterval() method to send multiple notifications based on timing.  Once user closed tab, notifications stopped.  In test, got many notifications saying 'Test!' since Notification.permission had become 'granted'.

--- static/js/desktop_notification.js
+++ static/js/desktop_notification.js
@@ -2,15 +2,39 @@
 
 const button = document.querySelector('#notify'); 
 
-button.addEventListener('click', (evt) => {
+// button.addEventListener('click', (evt) => {
+//     console.log('hello');
+//     if ('Notification' in window) {
+//         if (Notification.permission === 'granted') {
+//             // console.log('granted');
+
+//             // setTimeout(() => {
+//             //     //send notification
+//             // }, interval_in_milliseconds);
+
+//             const notification = new Notification('Test!');
+//         } else if (Notification.permission !== 'denied') {
+//             // console.log('ask for permission');
+//             Notification.requestPermission().then((permission) => {
+//                 if (permission === 'granted') {
+//                     // console.log('now granted');
+//                     const notification = new Notification('Test me');
+//                 }
+//             });
+//         }
+//     }
+// }
+// );
+
+function notify() {
     console.log('hello');
     if ('Notification' in window) {
         if (Notification.permission === 'granted') {
             // console.log('granted');
 
-            setTimeout(() => {
-                //send notification
-            }, interval_in_milliseconds);
+            // setTimeout(() => {
+            //     //send notification
+            // }, interval_in_milliseconds);
 
             const notification = new Notification('Test!');
         } else if (Notification.permission !== 'denied') {
@@ -23,6 +47,6 @@ button.addEventListener('click', (evt) => {
             });
         }
     }
+};
 
-
-});
+button.addEventListener('click', (evt) => {setInterval(notify, 2000);});
