c6c57737213600418e301cfdc5f00142783867fc
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 04:14:41 -0500

    Remove outdated function.  Tidy up comments

--- crud.py
+++ crud.py
@@ -219,51 +219,22 @@ def get_notification_by_id(notification_id):
 def get_notifications_to_send(current):
     """Return notifications to send."""
 
-    # `current` is when the scheduled send_push() function begins to run.
-    # Compare `current` to (the time the notification was last sent, plus the 
-    # days that a user is recommended to go before revisiting the exercise).
-    # If `current` is later in time in that comparison, then this 
-    # notification is due to be sent again, to encourage the user to revisit
-    # the exercise
+    # `current` is when the scheduled send_push() function begins to run
 
     notifs_to_send = []
     notifications = Notification.query.all()
     for notification in notifications:
+        # Calculate when notification should next be sent to prepare for 
+        # comparison with `current`
         next_date = notification.last_sent + timedelta(days=notification.exercise.frequency)
+        # If `current` is later in time than when the notification should
+        # next be sent, then this notification is due to be sent again, to 
+        # encourage the user to revisit the exercise
         if next_date < current:
             notifs_to_send.append(notification)
     # print(notifs_to_send)
 
     return notifs_to_send
-
-
-def get_notifications_to_send_for_test(current):
-    """Return notifications to deliver for testing purposes."""
-
-    # Problem solving in collaboration with Jared B: 
-    # get list of all notifications
-    # for each notification:
-    #   Check if that particular notification needs to be sent
-    #   get the date that this particular notification was LAST sent on
-    #   calculate the nextDate (when it should go out)
-    #   if nextDate < current:
-    #       include that notification in the list
-    #   compare that date to the current time
-    #   Add to a list if so
-    # Return all notifications in the list
-
-    notifs_to_send = []
-    notifications = Notification.query.all()
-    for notification in notifications:
-        next_date = notification.last_sent + timedelta(seconds=notification.exercise.frequency)
-        if next_date < current:
-            notifs_to_send.append(notification)
-    print(notifs_to_send)
-    return notifs_to_send
-
-    #print(Notification.query.filter((current - Notification.last_sent) > timedelta(seconds=Exercise.frequency)).all())
-                                                                                            # Works because SQLAlchemy knows which exercise based on notification having relationship to exercise
-    #return Notification.query.filter((current - Notification.last_sent) > timedelta(seconds=Exercise.frequency)).all()
 
 
 def get_subscriptions_from_notification(notification):
@@ -272,76 +243,8 @@ def get_subscriptions_from_notification(notification):
     subscriptions = PushSubscription.query.filter(notification.user_id == PushSubscription.user_id).all()
 
     return subscriptions
-    
-
-def get_users_notify():
-    """Return users to be notified."""
-
-    pass
-
-
-# # Deprecated
-# def get_users_for_push():
-#     """Return users who should get push notifications."""
-
-#     pacific_time = pytz.timezone("America/Los_Angeles")
-#     now = datetime.now(pacific_time)
-#     print(now)
-
-#     # A dictionary where keys are users and each value is a set of exercises 
-#     # about which that user should be notified.  Put exercises in set because 
-#     # in one scheduled push, we want each user to get one notification per 
-#     # exercise.
-#     users_exercises_push = {}
-
-#     # There could be efficiency improvements in going through all notifs
-#     for notification in Notification.query.all():
-#         print(notification.user)
-#         print(notification.exercise)
-
-#         # gap is a timedelta object
-#         gap = now - notification.last_sent
-#         print(gap)
-#         print(notification.exercise.frequency)
-#         print(timedelta(days=notification.exercise.frequency))
-
-#         if gap > timedelta(days=notification.exercise.frequency):
-#             users_exercises_push[notification.user] = users_exercises_push.get(notification.user, {"exercises": set()})["exercises"].add(notification.exercise)
-#             # Can an object, can an instance of a class be a dictionary key?  Immutable?
-#               # Make another key in inner dictionary - "subscription" and value is PushSubscription object(s)
-#               #     Was going to in function update_subscription_data_on_users_for_push()
-
-#         # Data structures are fun.  Am I making Python do too much?  
-#         # Use Object-Relational Mapping! 
-
-#     print(users_exercises_push)
-
-#     return users_exercises_push
-
-
-# # Deprecated
-# def update_subscription_data_on_users_for_push():
-#     """Return users who get notifications, with respective subscription data."""
-
-#     pass
 
 
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
-
-    # exercises_of_user = []
-    # for response in ResponseToPrompt.query.first().user.responses:
-    #     response.prompt.exercise =
-    #     for prompt in response.prompt:
-    #         print(prompt)
-    #         exercise = prompt.exercise
-    #         print(exercise)
-    #         if exercise not in exercises_of_user:
-    #             exercises_of_user.append(exercise)
-    # print(exercises_of_user)
-    # # print_exercise_responses(User.query.first(), choice(exercises_of_user))
-
-    # # print(ResponseToPrompt.query.first().user.responses)
-
-    # print_user_prompts(User.query.filter(User.user_id == 5))
