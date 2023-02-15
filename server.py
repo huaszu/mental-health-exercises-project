ef917b615616955a371e0f12d03336fe133e4807
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 17:56:31 -0500

    Refine comments

--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def show_create_account_page():
 
 @app.route("/exercises")
 def show_all_exercises():
-    """View all exercises."""
+    """View all journaling exercises."""
 
     exercises = crud.get_exercises()
 
@@ -86,7 +86,8 @@ def add_to_all_exercises():
 
     # Create prompt(s) within exercise
     for prompt_content in request.form.getlist("prompt"):
-        prompt = crud.create_prompt(prompt_content=prompt_content, exercise=exercise)
+        prompt = crud.create_prompt(prompt_content=prompt_content, 
+                                    exercise=exercise)
 
         db.session.add(prompt)
 
@@ -160,16 +161,16 @@ def show_user_exercises():
 
     try:
         user_id = session["user_id"]
-        print("\n\n\n\n\n", user_id)
-
         user = crud.get_user_by_id(user_id)
         exercises = crud.get_unique_exercises_of_user(user_id) # This is a set
 
-        return render_template("my_exercises.html", user=user, exercises=exercises)
+        return render_template("my_exercises.html", 
+                               user=user, 
+                               exercises=exercises)
     
     # If user is not logged in, say if user comes here by typing 
     # "/users/my_exercises" in the URL bar instead of by being redirected here
-    # from logging in.
+    # from logging in:
     except Exception as e:
         print(e)        
         flash("Log in to see exercises you have completed.")
@@ -181,13 +182,11 @@ def show_exercise(exercise_id):
 
     exercise = crud.get_exercise_by_id(exercise_id)
 
+    # Prepare information to help template display text to user about the 
+    # recommended frequency at which to complete this exercise
     day = "day"
     if exercise.frequency != 1:
         day = str(exercise.frequency) + " days"
-
-    # I don't think I need session.modified = True because session["user_id"] is
-    # what would change if user logs in on another tab.  We use 
-    # session.modified = True when we change a value in an inner dictionary (?)
 
     return render_template("exercise_details.html", exercise=exercise, day=day)
 
@@ -195,13 +194,13 @@ def show_exercise(exercise_id):
 def save_user_responses(exercise_id):
     """Create a new group of saved responses for the user for the exercise."""
     
-    # User would only get routed here if the user is logged in because of the 
-    # way static/js/alert_save_responses.js works.
-    
+    # User gets routed here only if the user is logged in because of the 
+    # code in `static/js/alert_save_responses.js`
+
+    # Associate one time with all responses from the same form, as opposed to
+    # a different time with each response from the same form
     pacific_time = pytz.timezone("America/Los_Angeles")
     time_completed_exercise = datetime.now(pacific_time)
-    # Associate one time with all responses from the same form, as opposed to
-    # a different time with each response from the same form.
     
     user_id = session["user_id"]
 
@@ -225,8 +224,6 @@ def save_user_responses(exercise_id):
 def get_login_status():
     """Get whether or not user is logged in."""
 
-    print("user_id:", session.get("user_id"))
-
     if "user_id" in session:
         return jsonify(True)
 
@@ -241,55 +238,15 @@ def process_logout():
 
 @app.route("/create")
 def create():
-    """Create an exercise."""
+    """Show user an interface to create an exercise."""
     
     # If user is not logged in, redirect user to log in.  Only logged in users
-    # can create exercises. 
+    # can create exercises
     if "user_id" not in session:
         flash("Please log in if you want to make an exercise.")
         return redirect("/") 
-        # Haha alternative: Show user 403 Permission Forbidden error
 
-    return render_template("create.html") # Can only get here if logged in
-    # Can let someone give themselves a pen name at this point!  Fix blank pen names later
-
-# Route currently not in use:
-@app.route("/service_worker.js")
-def allow_sw_scope_with_http_header():
-    """Override path restriction that limits scope of sw script."""
-
-    # Service Worker Script Response:
-    # An HTTP response to a service worker's script resource request can 
-    # include the following header:
-
-    # `Service-Worker-Allowed`
-    # Indicates the user agent will override the path restriction, which 
-    # limits the maximum allowed scope url that the script can control, to 
-    # the given value.
-
-    # Note: The value is a URL. If a relative URL is given, it is parsed 
-    # against the script’s URL.
-
-    # In addition to the origin restriction, service workers are restricted by 
-    # the path of the service worker script. For example, a service worker 
-    # script at https://www.example.com/~bob/sw.js can be registered for the 
-    # scope url https://www.example.com/~bob/ but not for the scope 
-    # https://www.example.com/ or https://www.example.com/~alice/. This 
-    # provides some protection for sites that host multiple-user content in 
-    # separated directories on the same origin. However, the path restriction 
-    # is not considered a hard security boundary, as only origins are. Sites 
-    # are encouraged to use different origins to securely isolate segments of 
-    # the site if appropriate.
-
-    # Servers can remove the path restriction by setting a 
-    # Service-Worker-Allowed header on the service worker script.
-    # https://www.w3.org/TR/service-workers/
-
-    response = make_response(send_from_directory(app.static_folder, "/js/service_worker.js"))
-    response.headers["Service-Worker-Allowed"] = "/"
-    response.headers["Content-Type"] = "application/javascript"
-    print(response)
-    return response
+    return render_template("create.html")
 
 @app.route("/vapid_public.json")
 def get_vapid_public_key():
@@ -298,39 +255,45 @@ def get_vapid_public_key():
     return jsonify(push_API_public_key)
 
 # In the case that the client has granted permission for push notifications, 
-# this route sets up the client for notifications going forward.
+# this route sets up the client for notifications going forward
 @app.route("/api/push-subscriptions", methods=["POST"])
 def initiate_push():
     """Create a subscription record if necessary and spawn first notification."""
 
-    # Subscription unique to a client
+    # A subscription is unique to a client
+    # A client has to have a subscription first before getting notifications
 
     user_id = session["user_id"]
     user = crud.get_user_by_id(user_id)
 
     json_data = request.json
-    subscription_json = json_data["subscription_json"]
-    print(subscription_json)
 
+    # Get the string containing the URL endpoint of the subscription.  Based on 
+    # https://developer.mozilla.org/en-US/docs/Web/API/PushSubscription/endpoint, 
+    # this endpoint takes the form of a custom URL that points to a push 
+    # server, which can be used to send a push message to the particular 
+    # service worker instance that subscribed to the push service
+    subscription_json = json_data["subscription_json"]
+
+    # Each notification is specific to an exercise
     exercise_id = int(json_data["exercise_id"])
     exercise = crud.get_exercise_by_id(exercise_id)
     
     # Check if there is already a matching PushSubscription object with the same 
-    # subscription_json because when we reload the page, even when the browser 
-    # resends the same subscription_json due to invoking the 
-    # registerServiceWorker function again, we will not cause an error on the 
-    # server. 
+    # `subscription_json` 
     subscription = crud.get_first_subscription(subscription_json=subscription_json)
 
-    # If there is no PushSubscription object with matching subscription_json, 
-    # create a new PushSubscription object.
+    # If there is no PushSubscription object with matching `subscription_json`, 
+    # create a new PushSubscription object
     if subscription is None:
         subscription = crud.create_push_subscription(subscription_json=subscription_json, 
                                                      user=user)
 
         db.session.add(subscription)
 
-    # Spawn first notification.
+    # Spawn first notification and record it.  
+    # Enables sending future notifications via a scheduled job that makes 
+    # calculation based on timing of previous notification
     send_first_push(subscription)
     pacific_time = pytz.timezone("America/Los_Angeles")
     last_sent = datetime.now(pacific_time)
@@ -353,6 +316,7 @@ if __name__ == "__main__":
     
     connect_to_db(app)
 
+    # Enable use of pytest testing framework
     if len(sys.argv) > 1 and sys.argv[1] == "--test":
         try:
             import pytest
