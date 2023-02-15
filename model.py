49c6ee9b8e410bbc8127a9438770383b58ae30a4
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 03:43:43 -0500

    Enhance code readability

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
 """Model for mental health exercises app."""
 
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import pytz
 
+# Initialize object
 db = SQLAlchemy()
 
 
@@ -20,10 +19,7 @@ class User(db.Model):
     first_name = db.Column(db.String(120), nullable=False)
     last_name = db.Column(db.String(120), nullable=False)
     pen_name = db.Column(db.String(120), nullable=True)
-    # If we have a default pen_name, then don't exactly need nullable=True.
-    # Better than having computation for if pen_name IS NULL, set default to
-    # first_name?
-    is_expert = db.Column(db.Boolean, nullable=False, default=False) # Not sure about default
+    is_expert = db.Column(db.Boolean, nullable=False, default=False)
     is_consumer = db.Column(db.Boolean, nullable=False, default=True)
 
     responses = db.relationship("ResponseToPrompt", back_populates="user") # A response belongs to one user
@@ -32,6 +28,7 @@ class User(db.Model):
     # Currently not accommodating: A push subscription can have more than one user, if different users have logged in on the same browser
     notifications = db.relationship("Notification", back_populates="user") # A notification belongs to one user
 
+    # Return string containing printable representation of object
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email} is_expert={self.is_expert}>'
 
@@ -46,13 +43,11 @@ class Exercise(db.Model):
                             primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    frequency = db.Column(db.Integer, nullable=True) # Theoretically could set a default
+    frequency = db.Column(db.Integer, nullable=True)
     time_limit_per_sitting = db.Column(db.Integer, nullable=True)
     author_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    # Having author_id as a column does not mean we need author_id to be a parameter in create_exercise() fn
 
     prompts = db.relationship("Prompt", back_populates="exercise") # A prompt belongs to one exercise
-    # Does prompts need to be a parameter in create_exercise() fn?  Theory: No, because when we create a prompt, we associate prompt to an exercise 
     author = db.relationship("User", back_populates="creations") # A user can author many exercises
     notifications = db.relationship("Notification", back_populates="exercise") # An exercise can have multiple notifications, for different users # Edge case example: If a user completes an exercise of freq 7 days today and then completes the same exercise tomorrow -> get notifications associated with both sittings
 
@@ -69,10 +64,8 @@ class Prompt(db.Model):
                           autoincrement=True,
                           primary_key=True)
     prompt_content = db.Column(db.Text, nullable=False)
-    prompt_type = db.Column(db.String(120), nullable=False, default="long answer") # V0: Let's say all prompts for now get free-form text response
-    # prompt_type values could be "short answer", "long answer", 
-    # "multiple choice - choose one", "multiple choice - choose multiple"
-    # Potentially in future: one prompt to many prompt options.  Another table
+    # V0 default: Let's say all prompts for now get free-form text response
+    prompt_type = db.Column(db.String(120), nullable=False, default="long answer")
     exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.exercise_id'), nullable=False)
 
     exercise = db.relationship("Exercise", back_populates="prompts") # An exercise can have many prompts
@@ -91,11 +84,11 @@ class ResponseToPrompt(db.Model):
                           autoincrement=True,
                           primary_key=True)
     response_content = db.Column(db.Text, nullable=True)
-    time_completed_exercise = db.Column(db.DateTime(timezone=True), nullable=True) # Should not be nullable?
+    time_completed_exercise = db.Column(db.DateTime(timezone=True), nullable=True)
     prompt_id = db.Column(db.Integer, db.ForeignKey('prompts.prompt_id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False) # is nullable what allows not signed in user to use?  Forget that!  Only create responses in db that have user_id
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
-    prompt = db.relationship("Prompt", back_populates="responses") # A prompt can have many responses, from different users or from the same user completing the same exercise multiple times
+    prompt = db.relationship("Prompt", back_populates="responses") # A prompt can have many responses, from different users and/or from the same user completing the same exercise multiple times
     user = db.relationship("User", back_populates="responses") # A user can have many responses
 
     def __repr__(self):
@@ -110,24 +103,20 @@ class PushSubscription(db.Model):
     id = db.Column(db.Integer,
                    autoincrement=True,
                    primary_key=True)
-    subscription_json = db.Column(db.Text, nullable=False) # unique?
+
+    # `subscription_json` is assigned a value that includes an "endpoint", an
+    # "expirationTime", and "keys" - such as a "p256dh" key and an "auth" key.
+    # An example of an "endpoint" may begin with 
+    # "https://fcm.googleapis.com/fcm/send/".
+    # `subscription_json` has the information that webpush() from pywebpush
+    # library takes in for the `subscription_info` parameter.
+    # https://developer.mozilla.org/en-US/docs/Web/API/PushSubscription
+    subscription_json = db.Column(db.Text, nullable=False)
+    # `user_id` is not nullable to enforce that a user has to be logged in to 
+    # sign up for notifications
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
     user = db.relationship("User", back_populates="push_subscriptions") # A user can have many push subscriptions, for instance if the same user signs in on many different browsers
-
-    # users = db.relationship("User", back_populates="push_subscriptions") # A user can have many push subscriptions
-    #   File "/Users/hsy/src/mental-health-exercises-project/env/lib/python3.10/site-packages/sqlalchemy/orm/decl_base.py", line 1142, in _declarative_constructor
-    # raise TypeError(
-    # TypeError: 'user' is an invalid keyword argument for PushSubscription
-
-    # We expect subscription_json to be assigned a value that includes an
-    # "endpoint", e.g., https://fcm.googleapis.com/fcm/send/ ... , an
-    # "expirationTime", and "keys" - such as a "p256dh" key and an "auth" key.
-
-    # subscription_json has the information that webpush takes in for the
-    # subscription_info parameter.
-
-    # https://developer.mozilla.org/en-US/docs/Web/API/PushSubscription/expirationTime
 
     def __repr__(self):
         return f'<PushSubscription id={self.id} subscription_json={self.subscription_json}>'
@@ -141,31 +130,16 @@ class Notification(db.Model):
     id = db.Column(db.Integer,
                    autoincrement=True,
                    primary_key=True)
-    last_sent = db.Column(db.DateTime(timezone=True), nullable=False) # don't want default?
+    last_sent = db.Column(db.DateTime(timezone=True), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.exercise_id'), nullable=False) # unique?  no because while each user should get only one notification for one exercise, multiple users can have notification enabled for that exercise
-    # freq = db.Column(db.Integer, db.ForeignKey('exercises.frequency'), nullable=False) # sqlalchemy.exc.ProgrammingError: (psycopg2.errors.InvalidForeignKey) there is no unique constraint matching given keys for referenced table "exercises" (Background on this error at: http://sqlalche.me/e/14/f405)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.exercise_id'), nullable=False)
 
-    user = db.relationship("User", back_populates="notifications") # A user can have many notifications | but user can have many push subscriptions and the notification sends to user via (a) subscription(s) - current solution: send through all of that user's subscriptions
-    exercise = db.relationship("Exercise", back_populates="notifications") # A notification belong to one exercise
-
-    # freq = exercise.frequency # raise AttributeError(key) AttributeError: frequency
+    user = db.relationship("User", back_populates="notifications") # A user can have many notifications
+    exercise = db.relationship("Exercise", back_populates="notifications") # A notification belongs to one exercise
 
     def __repr__(self):
         return f'<Notification id={self.id} last_sent={self.last_sent}>'
 
-# Notes on notif implementation:
-# Have to be logged in to sign up for notifs - right now enforce in that we do
-# not create a push subscription when there is no user_id -> look into the 
-# ROLLBACKs in terminal?
-
-# if logged in, don't worry about duplicates 
-# user 1
-# name of Exercise
-# frequency 
-# last notif sent
-# if > 7 days, send a new notif
-# update last notif column
 
 def connect_to_db(flask_app, db_uri="postgresql:///health", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
