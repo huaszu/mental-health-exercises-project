fc4518f8cb0de8af5a1e9c240982588b24b32d40
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 18:34:52 -0500

    Explain promptCounter

--- static/js/add_prompt.js
+++ static/js/add_prompt.js
@@ -1,9 +1,15 @@
 'use strict';
 
+// Allow user to add an additional prompt within an exercise.
+
+
 const button = document.querySelector('#add-prompt');
 
 const morePrompts = document.querySelector('#more-prompts');
 
+// The user interface initially gives the user room to fill out 1 prompt.
+// The first additional prompt would be the second prompt, the next 
+// additional would be the third, and so on. 
 let promptCounter = 2
 
 button.addEventListener('click', (evt) => {
