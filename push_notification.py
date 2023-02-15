72c2d3e2e7b2e00c272a34ff4ebe54055a2765f4
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 02:40:32 -0500

    Refine explanatory comments.  Remove print statements from debugging

--- push_notification.py
+++ push_notification.py
@@ -17,9 +17,10 @@ def send_first_push(subscription):
     """Push a notification.  Takes in a PushSubscription object."""
 
     result = "OK"
-    print(f"\n\n\n\n{result}")
 
     try:
+        # Encode `data`, add appropriate VAPID auth headers, and send
+        # to the push server identified in `subscription_info`
         webpush(
             subscription_info = json.loads(subscription.subscription_json), 
             data = json.dumps({"title": "*\O/* Ahoy",
@@ -27,6 +28,7 @@ def send_first_push(subscription):
             vapid_private_key = push_API_private_key, 
             vapid_claims = {"sub": push_API_subject}
         )
+    # Handle exception
     except WebPushException as ex:
         print(ex, repr(ex))
         if ex.response and ex.response.json():
@@ -37,92 +39,37 @@ def send_first_push(subscription):
                   extra.message)
         result = "FAILED"
 
-    print("ending result:", result)
-
     return result
-
-
-def send_push_for_test():
-    """Push next notification of each notification whose time is due, for testing purposes."""
-
-    # A current timestamp at the start of each time that this function runs, 
-    # which is more organized than creating different timestamps throughout
-    # various parts of the code within this function.  In one run of this job, 
-    # all notifications evaluated within the job are evaluated against the 
-    # same timestamp. 
-    pacific_time = pytz.timezone("America/Los_Angeles")
-    current = datetime.now(pacific_time)
-    print(current)
-
-    notifications_to_send = crud.get_notifications_to_send_for_test(current)
-    print(notifications_to_send)
-
-    for notification in notifications_to_send:
-        user = notification.user
-        exercise = notification.exercise
-        print("\n\n\n\n", "LOOK HERE", notification, user, exercise)
-
-        subscriptions = crud.get_subscriptions_from_notification(notification)
-        print(subscriptions)
-        for subscription in subscriptions:
-            result = "OK"
-            print(f"\n\n\n\n{result}")
-
-            try:
-                webpush(
-                    subscription_info = json.loads(subscription.subscription_json), 
-                    data = json.dumps({"title": f"*\O/* Ahoy, {user.first_name}",
-                                       "body": f"Time to do {exercise.title} CURRENT: {current}"}),
-                    vapid_private_key = push_API_private_key, 
-                    vapid_claims = {"sub": push_API_subject}
-                )
-            except WebPushException as ex:
-                print(ex, repr(ex))
-                if ex.response and ex.response.json():
-                    extra = ex.response.json()
-                    print("Remote service replied with a {}:{}, {}",
-                        extra.code,
-                        extra.errno,
-                        extra.message)
-                result = "FAILED"
-
-            print("ending result:", result)
-        
-        notification.last_sent = current
-        db.session.commit()
-
-    print("Job succeeded")
-
-    # return
 
 
 def send_push():
     """Push next notification of each notification whose time is due."""
 
-    # A current timestamp at the start of each time that this function runs, 
-    # which is more organized than creating different timestamps throughout
-    # various parts of the code within this function.  In one run of this job, 
-    # all notifications evaluated within the job are evaluated against the 
-    # same timestamp. 
+    # Store current timestamp in Pacific Time.  Using a current timestamp at 
+    # the start of each time that this function runs is more organized than 
+    # creating different timestamps throughout various parts of the code 
+    # within this function.  In one run of this job, evaluate all 
+    # notifications within the job against the same timestamp. 
     pacific_time = pytz.timezone("America/Los_Angeles")
     current = datetime.now(pacific_time)
-    print(current)
 
+    # Get notifications whose time is due to be sent again.
     notifications_to_send = crud.get_notifications_to_send(current)
-    print(notifications_to_send)
 
     for notification in notifications_to_send:
+        # Identify the user and the exercise of this notification
         user = notification.user
         exercise = notification.exercise
-        print(notification, user, exercise)
 
+        # Get subscription(s) corresponding to this notification
         subscriptions = crud.get_subscriptions_from_notification(notification)
-        print(subscriptions)
+
         for subscription in subscriptions:
             result = "OK"
-            print(f"\n\n\n\n{result}")
 
             try:
+                # Encode `data`, add appropriate VAPID auth headers, and send
+                # to the push server identified in `subscription_info`
                 webpush(
                     subscription_info = json.loads(subscription.subscription_json), 
                     data = json.dumps({"title": f"*\O/* Ahoy, {user.first_name}",
@@ -130,6 +77,7 @@ def send_push():
                     vapid_private_key = push_API_private_key, 
                     vapid_claims = {"sub": push_API_subject}
                 )
+            # Handle exception
             except WebPushException as ex:
                 print(ex, repr(ex))
                 if ex.response and ex.response.json():
@@ -139,13 +87,11 @@ def send_push():
                         extra.errno,
                         extra.message)
                 result = "FAILED"
-
-            print("ending result:", result)
         
+        # Update information on when this notification was last sent, which
+        # is used to compute which notifications are due to be sent again
         notification.last_sent = current
+
         db.session.commit()
 
     print("Job succeeded")
-
-# Timing of push notification delivery currently based on when user opted in, 
-# and not also related to datetime of user's past completion of that exercise.
