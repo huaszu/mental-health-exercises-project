0af0e239cc3eb467554bfc21bcbff83889720d4e
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 18:30:18 -0500

    Add comment

--- static/js/desktop_notification.js
+++ static/js/desktop_notification.js
@@ -1,52 +1,26 @@
 'use strict';
 
+// Listen for click on "notify" button to check whether client has granted
+// permission to send notifications and, if so, to send a test notification
+
+
 const button = document.querySelector('#notify'); 
 
-// button.addEventListener('click', (evt) => {
-//     console.log('hello');
-//     if ('Notification' in window) {
-//         if (Notification.permission === 'granted') {
-//             // console.log('granted');
-
-//             // setTimeout(() => {
-//             //     //send notification
-//             // }, interval_in_milliseconds);
-
-//             const notification = new Notification('Test!');
-//         } else if (Notification.permission !== 'denied') {
-//             // console.log('ask for permission');
-//             Notification.requestPermission().then((permission) => {
-//                 if (permission === 'granted') {
-//                     // console.log('now granted');
-//                     const notification = new Notification('Test me');
-//                 }
-//             });
-//         }
-//     }
-// }
-// );
 
 function notify() {
     console.log('hello');
     if ('Notification' in window) {
         if (Notification.permission === 'granted') {
-            // console.log('granted');
-
-            // setTimeout(() => {
-            //     //send notification
-            // }, interval_in_milliseconds);
-
             const notification = new Notification('Test!');
         } else if (Notification.permission !== 'denied') {
-            // console.log('ask for permission');
             Notification.requestPermission().then((permission) => {
                 if (permission === 'granted') {
-                    // console.log('now granted');
                     const notification = new Notification('Test me');
                 }
             });
         }
     }
 };
+
 
 button.addEventListener('click', (evt) => {setInterval(notify, 2000);});
