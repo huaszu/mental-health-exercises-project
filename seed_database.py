577e7e94b5a8718c8787599ddbd35d20d45dd024
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-02-15 03:09:08 -0500

    Improve comments

--- seed_database.py
+++ seed_database.py
@@ -14,22 +14,25 @@ import model
 import server
 import werkzeug.security
 
+# Remove the existing database called "health"
 os.system("dropdb health")
+
+# Create an empty database called "health"
 os.system("createdb health")
 
 model.connect_to_db(server.app)
 model.db.create_all()
 
 
-# Create test users.
+# Create test users
 
 for n in range(10):
-    email = f"user{n}@test.com"  # Voila! A unique email!
+    email = f"user{n}@test.com" # Voila! A unique email!
     password = "test"
     first_name = f"Fname{n}"
     last_name = f"Lname{n}"
     pen_name = f"Mysterious{n}"
-    # Seed some users who are only Experts, some who are only Consumers, and some who are both.
+    # Seed some users who are only Experts, some who are only Consumers, and some who are both
     if n in range(0, 4): # Users 0, 1, 2, 3
         is_expert = True
         is_consumer = False
@@ -40,8 +43,10 @@ for n in range(10):
         is_expert = True
         is_consumer = True
     
+    # Hash password for security
     hashpw = werkzeug.security.generate_password_hash(password)
 
+    # Create user
     user = crud.create_user(email=email, 
                             password=hashpw, 
                             first_name=first_name,
@@ -49,26 +54,30 @@ for n in range(10):
                             is_expert=is_expert,
                             is_consumer=is_consumer,
                             pen_name=pen_name)
+
+    # Add new record.  `db.session` stores planned modifications to database                        
     model.db.session.add(user)
+
+# Commit transaction
 model.db.session.commit()
 
-# Prepare list of respondents to respond to exercises to be created. 
 
+# Prepare list of respondents who make test responses to exercises
 respondents = User.query.filter(User.is_consumer == True).all()
 
-# Create renowned experts and author their exercises.
+# Create renowned expert users and author their exercises.
 
+# Prompts can have four intended ways to respond
 prompt_types = ["short answer", 
-        "long answer", 
-        "multiple choice - choose one", 
-        "multiple choice - choose multiple"]
+                "long answer", 
+                "multiple choice - choose one", 
+                "multiple choice - choose multiple"]
 
-for name, val in seed_experts.items(): # val is a dict with key "exercises" and value being a dict
-    # Create renowned experts.
-
-    # .split() splits on whitespace by default
+# `val` is a dictionary with key "exercises" and value being a dictionary
+for name, val in seed_experts.items():
     split_name = name.split()
-    email_local_part = "".join(split_name) # e.g., Brené Brown's email_local_part is BrenéBrown
+    # e.g., Brené Brown's `email_local_part` == BrenéBrown
+    email_local_part = "".join(split_name)
     email = email_local_part + "@test.com"
     password = "test"
     first_name = split_name[0]
@@ -77,8 +86,10 @@ for name, val in seed_experts.items(): # val is a dict with key "exercises" and 
     is_expert = True
     is_consumer = True
 
+    # Hash password for security
     hashpw = werkzeug.security.generate_password_hash(password)
 
+    # Create user who is an expert
     user = crud.create_user(email=email, 
                             password=hashpw, 
                             first_name=first_name,
@@ -88,11 +99,15 @@ for name, val in seed_experts.items(): # val is a dict with key "exercises" and 
                             pen_name=pen_name)
     model.db.session.add(user)
 
-    # Seed exercises, authored by users who are renowned experts. 
+    # Create exercises, authored by this user 
 
-    for exercise_title in val["exercises"]: # val["exercises"] is a list of exercise titles
+    # `val["exercises"]` is a list of exercise titles
+    for exercise_title in val["exercises"]:
         title = exercise_title
-        description = seed_exercise_details[exercise_title]["description"] # seed_exercise_details[exercise_title] is a dict containing keys of "description", "frequency", "time_limit_per_sitting", and "prompts"
+        # `seed_exercise_details[exercise_title]` is a dictionary containing 
+        # keys of "description", "frequency", "time_limit_per_sitting", 
+        # and "prompts"
+        description = seed_exercise_details[exercise_title]["description"] 
         frequency = int(seed_exercise_details[exercise_title]["frequency"])
         time_limit_per_sitting = int(seed_exercise_details[exercise_title]["time_limit_per_sitting"])
         author = User.query.filter(User.pen_name == name).first()
@@ -105,12 +120,16 @@ for name, val in seed_experts.items(): # val is a dict with key "exercises" and 
 
         model.db.session.add(exercise)
 
-        # Create prompts for every seed exercise.
+        # Create prompts for every seed exercise
 
         prompt_type = "long answer"
 
-        # seed_exercise_details[exercise.title] is a dict containing keys of "description", "frequency", "time_limit_per_sitting", and "prompts"
-        for prompt_str in seed_exercise_details[exercise.title]["prompts"]: # seed_exercise_details[exercise.title]["prompts"] is a list of strings that are prompts
+        # `seed_exercise_details[exercise.title]` is a dictionary containing 
+        # keys of "description", "frequency", "time_limit_per_sitting", 
+        # and "prompts".
+        # `seed_exercise_details[exercise.title]["prompts"]` is a list of 
+        # strings that are prompts.
+        for prompt_str in seed_exercise_details[exercise.title]["prompts"]:
             prompt_content = prompt_str
 
             prompt = crud.create_prompt(prompt_content=prompt_content, 
@@ -119,20 +138,23 @@ for name, val in seed_experts.items(): # val is a dict with key "exercises" and 
             
             model.db.session.add(prompt)
 
-            # Create 2 test responses for each prompt of each exercise. 
+            # Create 2 test responses for each prompt of each exercise 
 
             pacific_time = pytz.timezone("America/Los_Angeles")
             time_completed_exercise = datetime.now(pacific_time)
 
             response_content = "Response"
 
+            # Randomly choose a respondent
             user1 = choice(respondents)
             response1 = crud.create_response(response_content=response_content, 
                                              prompt=prompt, 
                                              user=user1,
                                              time_completed_exercise=time_completed_exercise)
-
-            user2 = choice(respondents) # It is possible for the same user to have multiple responses to the same prompt, from doing that exercise on different occasions. 
+            
+            # Allow for the same user to have multiple responses to the same 
+            # prompt because a user can do an exercise on multiple occasions 
+            user2 = choice(respondents) 
             response2 = crud.create_response(response_content=response_content, 
                                              prompt=prompt, 
                                              user=user2,
@@ -140,7 +162,15 @@ for name, val in seed_experts.items(): # val is a dict with key "exercises" and 
 
             model.db.session.add_all([response1, response2])
 
-            # Since we seed responses for each prompt, it is possible for a user to respond to a subset of and not all prompts within an exercise.  The same users randomly picked for one prompt may not get picked again for the next prompt of the same exercise.
+            # In this way of seeding responses for each prompt, it is possible 
+            # for a user to respond to a subset of and not all prompts within 
+            # an exercise.  A user randomly selected for one prompt may not 
+            # get selected again for the next prompt of the same exercise, 
+            # which aligns with how the app allows users to make 
+            # submissions in which not all prompts have a response and which
+            # aligns with reality of how users want to use the app, based on
+            # user research.
 
+# Committing a transaction takes a relatively long time so better to commit 
+# once outside loop 
 model.db.session.commit()
-# Commit takes a long time so better to commit once outside loop 
